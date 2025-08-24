e97f98fe550a947ff9fe839ab33256aff21fc082
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-24 13:23:08 +0500

    first commit

---
+++ Day07/main.py
@@ -0,0 +1,19 @@
+# import os
+
+# path = "/"
+# entries = os.listdir(path)
+# for entry in entries:
+#     full_path = os.path.join(path, entry)
+#     if os.path.isfile(full_path):
+#         print(entry)
+
+
+#pracitce start from code with harry video..
+# a=6
+# b=3
+# c="huzaifa"
+# print("the answer is",a+b)
+# print("My name is",c,"the answer is",a-b)
+# print("hey how are u guys")
+
+    
---
+++ Day08/pracitcequestions.py
@@ -0,0 +1,13 @@
+# Write a python program to add two numbers.
+
+# num1= int(input("Enter number 1: "))
+# num2= int(input("Enter number 2: "))
+# num = num1+num2
+# print("Addition of both numbers is: ",num)
+
+# Write a python program to find remainder when a number is divided by z.
+
+num1 = float(input("enter a value to find a reminder: "))
+num2 = float(input("enter a number to which the value should divide: "))
+num3 = num1%num2
+print("Reminder: ",num3)
