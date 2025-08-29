cef024b3b7c231264da754f2b9345ca7474d037a
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-29 12:55:29 +0500

    Day 9 end

--- Day09/practiceset.py
+++ Day09/practiceset.py
@@ -3,11 +3,26 @@
 # name = input("Enter your name: ")
 # print(f"Good Afternoon ! {name}")
 
-# 2.
-# Write a program to fill in a letter template given below with name and date.
-letter ='''
-Dear <|Name|> ,
-You are selected!
-<|Date|>
-'''
-print(letter.replace("<|Name|>","huzaifa").replace("<|Date|>","29-08-2025"))
+# 2. Write a program to fill in a letter template given below with name and date.
+
+# letter ='''
+# Dear <|Name|> ,
+# You are selected!
+# <|Date|>
+# '''
+# print(letter.replace("<|Name|>","huzaifa").replace("<|Date|>","29-08-2025"))
+
+# 3. Write a program to detect double space in a string.
+
+# name = "My name is  Huzaifa"
+# print(name.find("  "))
+
+# # 4. eplace the double spacefrom problem 3 with single spaces.
+
+# print(name.replace("  "," "))
+# print(name) # Strings are immutable which means that you cannot change them by running functions on them
+
+# 5. Write a program to format the following letter using escape sequence characters.
+# Letter ="Dear Harry, this python course is nice. Thanks! "
+
+Letter = print("\"Dear Harry,\n\tThis python course is nice.\nThanks!\"")
