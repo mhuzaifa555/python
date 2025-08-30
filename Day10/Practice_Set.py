9ba96d77edb0a4a5a0035018aa1bfe93e5fcc7a2
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-30 23:43:19 +0500

    Day 10

---
+++ Day10/Practice_Set.py
@@ -0,0 +1,43 @@
+# 1: Write program to Store fruits in a list entered by user.
+# fruits = []
+# fruit1 = input("enter a fruit name: ")
+# fruits.append(fruit1)
+# fruit2 = input("enter a fruit name: ")
+# fruits.append(fruit2)
+# fruit3 = input("enter a fruit name: ")
+# fruits.append(fruit3)
+# fruit4 = input("enter a fruit name: ")
+# fruits.append(fruit4)
+# fruit5 = input("enter a fruit name: ")
+# fruits.append(fruit5)
+# fruit6 = input("enter a fruit name: ")
+# fruits.append(fruit6)
+# fruit7 = input("enter a fruit name: ")
+# fruits.append(fruit7)
+# print(fruits)
+
+# 2. Write a program to accept marks of 5 students and display them in a sorted manner.
+# marks = []
+# Student1 = input("Enter the Marks of Student 1: ")
+# marks.append(Student1)
+# Student2 = input("Enter the Marks of Student 2: ")
+# marks.append(Student2)
+# Student3 = input("Enter the Marks of Student 3: ")
+# marks.append(Student3)
+# Student4 = input("Enter the Marks of Student 4: ")
+# marks.append(Student4)
+# Student5 = input("Enter the Marks of Student 5: ")
+# marks.append(Student5)
+# print(marks)
+# marks.sort()
+# print(marks)
+
+# 4. Write a program to sum a list with 4 numbers.
+
+# list = (23,32,3,55,55)
+# print(sum(list))
+
+# 5. Write a program to count the number ot zeros in the tallowins tuple:
+
+# list = (0,0,0,2,23,3,4,5,0,0)
+# print(list.count(0))
---
+++ Day10/list.py
@@ -0,0 +1,12 @@
+l1 = ["Huzaifa",20,10.5,"Omer",True,"pakistan"]
+# # print(l1[1])
+# # print(l1[0:6])
+# l1[0]="omer"
+# # print(l1[0])
+# l1.append("Huzaifa")
+# l1.pop(2)
+# # print(l1[len])
+# l1.insert(6,"sixth")
+# print(l1)
+l1.reverse()
+print(l1)
