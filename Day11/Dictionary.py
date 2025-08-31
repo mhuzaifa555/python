a8ba8fb11d3373f02731c5fb136906a838fa7238
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-31 20:27:24 +0500

    Day 11

---
+++ Day11/Dictionary.py
@@ -0,0 +1,37 @@
+Bio = {
+    "name" : "Huzaifa",
+    "Fname" : "Zahid Shafique",
+    "Contact" :92031231231,
+    "Address" : "E-55 Khosa Goth",
+    "age" : 20,
+}
+
+# print(Bio)
+# Bio.update({"name" : "Huzaifa Zahid" , "Education":"Undergraduate"})
+# print(Bio)
+# print(Bio.get("age"))
+# print(Bio.get("namee"))
+# print(Bio.values())
+# print(Bio.keys())
+# print(Bio.items())
+# removed  = Bio.pop ("Address")    Removes a key and returns its value.
+# print(removed)
+
+# Q1: Create a dictionary of 5 students with their marks.Print the name of the student who has the highest marks.
+# students = {
+#     "Student1" : 30,
+#     "Student2" : 60,
+#     "Student3" : 10,
+#     "Student4" : 80,
+#     "Student5" : 70,
+# }
+# highest = students.values()
+# print(max(highest))
+
+# Q2 You have a dictionary: fruits = {"apple": 3, "banana": 5, "orange": 2}
+# Add a new fruit "mango": 7 Then remove "banana" from the dictionary.
+
+# fruits = {"apple": 3, "banana": 5, "orange": 2}
+# fruits.update({"mango":7})
+# fruits.pop("banana")
+# print(fruits)
