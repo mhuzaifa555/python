62c14e2384f18d6138d14a674c3377ffdf8dfeab
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-09-29 21:37:42 +0500

    After Break

---
+++ AfterPapaperDay1/conditional_statments.py
@@ -0,0 +1,7 @@
+age = int(input("Enter age please!: "))
+if (age>18):
+    print("you can access this site!: ")
+elif (age<=0 or age >150):
+    print("Invalid age entered")
+else:
+    print("you cant access this site")
