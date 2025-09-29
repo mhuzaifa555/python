2465d99e5ef9c9adc127d4db46ab13cece355af3
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-09-29 22:54:45 +0500

    More practice of conditional statments

--- AfterPapaperDay1/conditional_statments.py
+++ AfterPapaperDay1/conditional_statments.py
@@ -1,7 +1,11 @@
 age = int(input("Enter age please!: "))
-if (age>18):
-    print("you can access this site!: ")
-elif (age<=0 or age >150):
-    print("Invalid age entered")
+# if (age>18):
+#     print("you can access this site!: ")
+# elif (age<=0 or age >150):
+#     print("Invalid age entered")
+# else:
+#     print("you cant access this site")
+if (age>=18):
+    print("yes")
 else:
-    print("you cant access this site")
+    print("no")
---
+++ AfterPapaperDay1/practice_set.py
@@ -0,0 +1,49 @@
+# # 1. Write a program to find the greatest of four numbers entered by the user.
+
+# a = int(input("enter no 1: "))
+# b = int(input("enter no 2: "))
+# c = int(input("enter no 3: "))
+# d = int(input("enter no 4: "))
+
+# if(a>b and a>c and a>d):
+#     print("No 1 is the greatest: ",a)
+# elif(b>a and b>c and b>d):
+#     print("No 2 is the greatest: ",b)
+# elif(c>b and c>a and c>d):
+#     print("No 3 is the greatest: ",c)
+# elif(d>b and d>c and d>a):
+#     print("No 4 is the greatest: ",d)
+
+# 2. write a progrem to find out whether a student failed if it requires a total of 40% and at least 33% in each subject to pass. 
+# Assume 3 subjects and take marks as an input from the user.
+
+# Maths = float(input("Enter Subject 1 marks: "))
+# English = float(input("Enter Subject 2 marks: "))
+# Computer = float(input("Enter Subject 3 marks: "))
+
+# marks = (300/Maths+English+Computer)*100
+
+# if(marks >=40 and Maths >=33 and English >=33 and Computer >=33):
+#     print("Passed!")
+# else:
+#     print("Failed!")
+
+# 3. A spam comment is defined as a text containing following keywords: 
+# "Make a lot of money", "buy now". "subscribe this". "click this". Write a program to detect these spams.
+
+# comment = input("Write a comment: ")
+# spam1 = "Make a lot of money"
+# spam2 = "buy now"
+# spam3 = "subscribe this"
+# spam4 =  "click this"
+# if(spam1 in  comment or spam2 in comment or spam3 in comment or spam4 in comment):
+#     print("Its a spam comment!")
+# else:
+#     print("Not a Spam Comment")
+
+# 4. Write a program to find whether a gwen username contains lgss than 10 characters or not.
+# user = input("Create a username: ")
+# if(len(user)< 10):
+#     print("Username is less than 10 characters")
+# else:
+#     print("Username have more than 10 characters")
